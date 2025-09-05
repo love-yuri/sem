b379e4edfe50e20581fa6c4bba55da77446fc077
love-yuri <yuri2078170658@gmail.com>
2025-09-05 08:56:06 +0800

    build: 添加构建脚本以编译服务器和UI项目

---
+++ AppData/build.bat
@@ -0,0 +1,4 @@
+& "D:\Visual Studio\ide\MSBuild\Current\Bin\amd64\MSBuild.exe" server.sln /p:Configuration=Release /p:OutputPath=E:\work\sem\build
+
+
+& "D:\Visual Studio\ide\MSBuild\Current\Bin\amd64\MSBuild.exe" ui.sln /p:Configuration=Release /p:OutputPath=E:\work\sem\build
